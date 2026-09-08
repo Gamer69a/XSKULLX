50a09d51cb9d2221b92d90101b5e995e867621cd
Hansal <hansalhhprajapati2303@gmail.com>
2026-09-08 10:54:11 -0400

    Fix ES module imports

--- automator.js
+++ automator.js
@@ -1,6 +1,6 @@
-const { execSync } = require('child_process');
-const fs = require('fs');
-const https = require('https');
+import { execSync } from 'child_process';
+import fs from 'fs';
+import https from 'https';
 
 // CONFIGURATION
 const GITHUB_USERNAME = process.env.GITHUB_USERNAME || 'Gamer69a';
